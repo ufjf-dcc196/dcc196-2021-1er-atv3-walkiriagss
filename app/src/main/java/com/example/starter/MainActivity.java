f38df19310bb0ceffdd2929bd3451d16239413b8
walkiria <walkiria.garcia@engenharia.ufjf.br>
2021-06-27 22:01:29 -0300

    Criando função clickBotao. Mudar saída para "Boatão clicado!"

--- app/src/main/java/com/example/starter/MainActivity.java
+++ app/src/main/java/com/example/starter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.starter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
         //código
         TextView saida = findViewById(R.id.saida); //
         saida.setText("Bem vinda!");
+
+    }
+
+    public void clickBotao(View view){
+        TextView saida = findViewById(R.id.saida); //
+        saida.setText("Botão clicado!");
 
     }
 }
