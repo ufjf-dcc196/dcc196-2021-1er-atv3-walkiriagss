5b693bc7bf1defc2a453266efd9bf4bb1e89f74a
walkiria garcia <walkiria.garcia@engenharia.ufjf.br>
2021-06-28 00:30:01 -0300

    Tratamento para quando os campos dos números vazios

--- app/src/main/java/com/example/starter/MainActivity.java
+++ app/src/main/java/com/example/starter/MainActivity.java
@@ -22,16 +22,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //código
-        saidaCalculo = findViewById(R.id.saida); //
+        saidaCalculo = findViewById(R.id.saida);
+        saidaCalculo.setText("Cálculo = 0,00");
         numero1 = findViewById(R.id.editTextNumber1);
         numero2 = findViewById(R.id.editTextNumber2);
         botaoSoma = findViewById(R.id.buttonSoma);
     }
 
     public void soma(View view){
-        Double numberNumero1 = Double.parseDouble(numero1.getText().toString());
-        Double numberNumero2 = Double.parseDouble(numero2.getText().toString());
-        Double numberSaida = numberNumero1 + numberNumero2;
-        saidaCalculo.setText((numberSaida.toString()));
-    }
+        Double numberNumero1 = 0.0;
+        Double numberNumero2 = 0.0;
+        try {
+            numberNumero1 = Double.parseDouble(numero1.getText().toString());
+            numberNumero2 = Double.parseDouble(numero2.getText().toString());
+            Double numberSaida = numberNumero1 + numberNumero2;
+            saidaCalculo.setText(("Cálculo = " + numberSaida.toString()));
+        }catch (Exception e){
+            saidaCalculo.setText("Digite os dois números");
+        }
+
+        }
 }
